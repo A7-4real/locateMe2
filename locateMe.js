8eac9567a48ce68a54601101c5f3da57fbbd4c0a
A7-4real <a7.4real@gmail.com>
2022-02-16 00:11:21 +0530

    added distance( ) function
    distance function for finding distance between guess coordinates and original coordinates

--- locateMe.js
+++ locateMe.js
@@ -195,6 +195,34 @@ function check() {
   disableButton("check");
 }
 
+function distance(lat1, lon1, lat2, lon2, unit) {
+  if (lat1 == lat2 && lon1 == lon2) {
+    return 0;
+  } else {
+    var radlat1 = (Math.PI * lat1) / 180;
+    var radlat2 = (Math.PI * lat2) / 180;
+    var theta = lon1 - lon2;
+    var radtheta = (Math.PI * theta) / 180;
+    var dist =
+      Math.sin(radlat1) * Math.sin(radlat2) +
+      Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta);
+    if (dist > 1) {
+      dist = 1;
+    }
+
+    dist = Math.acos(dist);
+    dist = (dist * 180) / Math.PI;
+    dist = dist * 60 * 1.1515;
+    if (unit == "K") {
+      dist = dist * 1.609344;
+    }
+    if (unit == "N") {
+      dist = dist * 0.8684;
+    }
+    return (dist / 1.609).toFixed(1);
+  }
+}
+
 var index = -1;
 function randomLoc() {
   index += 1;
@@ -231,6 +259,19 @@ function randomLoc() {
   }
 }
 
+function display_location() {
+  document.getElementById("location").innerHTML =
+    "Correct Location: " + current_name;
+  document.getElementById("distance").innerHTML =
+    "Your Guess was " + distance_from_guess + " Miles away";
+  document.getElementById("totaldistance").innerHTML =
+    "Round Score: " + accumulated_distance.toFixed(1) + " Miles";
+}
+
 function disableButton(id) {
   document.getElementById(id).disable = true;
+}
+
+function enableButton(id) {
+  document.getElementById(id).disable = false;
 }
